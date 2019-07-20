167334053c23f0ffcd6985dbf677559ff69b3041
Andrew Chang <andrewhychang@gmail.com>
2019-07-20 11:35:31 +1000

    added videoplayer functionality, no audio or nav yet. runs

--- borderlessplayer.py
+++ borderlessplayer.py
@@ -1,9 +1,12 @@
 import gi
+import pafy
+import vlc
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, GLib
 
 class BorderlessWindow(Gtk.Window):
-	
+	global VIDURL
+
 	def __init__(self):
 		Gtk.Window.__init__(self, title="Borderless Player")
 		self.set_size_request(300,50)
@@ -21,13 +24,28 @@ class BorderlessWindow(Gtk.Window):
 
 
 	def parseUrl(self, entry):
-		url = entry.get_text()
-		print(url)
+		"""
+		#VIDURL = entry.get_text()
+		VIDURL = "https://www.youtube.com/watch?v=Pv_5VTrHyqY"
+		videoWindow = pafy.new(VIDURL)
+		best = videoWindow.getbestaudio()
+		playurl = best.VIDURL
+		newvlc = vlc.MediaPlayer(playurl)
+		player.play()
+		while True:pass
+		"""
+		url = entry.get_text()                                                                                        
+		myvideo = pafy.new(url)                                                                                                                       
+		bestvid = myvideo.getbestvideo()                                                                                                                 
+		bestVidUrl = bestvid.url                                                                                                                          
+		player = vlc.MediaPlayer(bestVidUrl)                                                                                                           
+		player.play()
+		while True:pass
+
+class VideoWindow(Gtk.Window):
+	mystring = "String"
 
 win = BorderlessWindow()
 win.connect("destroy", Gtk.main_quit)
 win.show_all()
-win2 = BorderlessWindow()
-win2.connect("destroy", Gtk.main_quit)
-win2.show_all()
 Gtk.main()
