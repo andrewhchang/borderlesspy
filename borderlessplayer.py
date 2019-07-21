23485f0449d457f630eda673ac10efdf9b2c9904
Andrew Chang <andrewhychang@gmail.com>
2019-07-21 11:27:22 +1000

    started adding navigation implementation. not running as of yet

--- borderlessplayer.py
+++ borderlessplayer.py
@@ -24,26 +24,89 @@ class BorderlessWindow(Gtk.Window):
 
 
 	def parseUrl(self, entry):
-		"""
-		#VIDURL = entry.get_text()
-		VIDURL = "https://www.youtube.com/watch?v=Pv_5VTrHyqY"
-		videoWindow = pafy.new(VIDURL)
-		best = videoWindow.getbestaudio()
-		playurl = best.VIDURL
-		newvlc = vlc.MediaPlayer(playurl)
-		player.play()
-		while True:pass
-		"""
-		url = entry.get_text()                                                                                        
-		myvideo = pafy.new(url)                                                                                                                       
-		bestvid = myvideo.getbestvideo()                                                                                                                 
-		bestVidUrl = bestvid.url                                                                                                                          
-		player = vlc.MediaPlayer(bestVidUrl)                                                                                                           
+		url = entry.get_text() #retrieve url from entry box                                                                                       
+		myvideo = pafy.new(url) #create new pafy video url                                                                                                                      
+		
+		
+		#alter pafy url to play vid at best quality
+		bestvid = myvideo.getbest()                                                                                                                 
+		bestVidUrl = bestvid.url
+
+		#create new vlc instance with content = pafy url                                                                                                                         
+		#myPlayer = BorderlessPlayer()
+		player = BorderlessPlayer(bestVidUrl)
+		player.set_fullscreen(False)                                                                                                     
 		player.play()
 		while True:pass
 
-class VideoWindow(Gtk.Window):
-	mystring = "String"
+class BorderlessPlayer(vlc.MediaPlayer):
+	def __init__(self):
+		self.vlc_instance = vlc.MediaPlayer(
+            "--no-video-deco " +
+            "--no-embedded-video " +
+            "--video-x=X  " + 
+            "--video-y=Y  " 
+		)
+
+class NavWindow(Gtk.Window):
+
+	def __init__(self):
+		Gtk.Window.__init__(self, title="Python-Vlc Media Player")
+
+		self.playback_button = Gtk.Button()
+        self.stop_button = Gtk.Button()
+        self.play_image = Gtk.Image.new_from_icon_name(
+                "gtk-media-play",
+                Gtk.IconSize.MENU
+            )
+        self.pause_image = Gtk.Image.new_from_icon_name(
+                "gtk-media-pause",
+                Gtk.IconSize.MENU
+            )
+        self.stop_image = Gtk.Image.new_from_icon_name(
+                "gtk-media-stop",
+                Gtk.IconSize.MENU
+            )
+
+        def toggle_player_playback(self, widget, data=None):
+	        if self.is_player_active == False and self.player_paused == False:
+	            self.player.play()
+	            self.playback_button.set_image(self.pause_image)
+	            self.is_player_active = True
+
+	        elif self.is_player_active == True and self.player_paused == True:
+	            self.player.play()
+	            self.playback_button.set_image(self.pause_image)
+	            self.player_paused = False
+
+	        elif self.is_player_active == True and self.player_paused == False:
+	            self.player.pause()
+	            self.playback_button.set_image(self.play_image)
+	            self.player_paused = True
+	        else:
+	            pass
+
+	        self.playback_button.set_image(self.play_image)
+	        self.stop_button.set_image(self.stop_image)
+	        self.playback_button.connect("clicked", self.toggle_player_playback)
+	        self.stop_button.connect("clicked", self.stop_player)
+
+        def stop_player(self, widget, data=None):
+	        self.player.stop()
+	        self.is_player_active = False
+	        self.playback_button.set_image(self.play_image)
+
+		    self.draw_area.connect("realize",self._realized)
+			self.hbox = Gtk.Box(spacing=6)
+	        self.hbox.pack_start(self.playback_button, True, True, 0)
+	        self.hbox.pack_start(self.stop_button, True, True, 0)
+
+	        self.vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+	        self.add(self.vbox)
+	        self.vbox.pack_start(self.draw_area, True, True, 0)
+	        self.vbox.pack_start(self.hbox, False, False, 0)
+
+	        self.show_all()
 
 win = BorderlessWindow()
 win.connect("destroy", Gtk.main_quit)
